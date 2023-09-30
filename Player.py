6b9351b6dfa257046aa97e3bb20a8c13814f3ac2
parmartarun <parmartarun.dev@gmail.com>
2023-09-30 12:25:06 -0400

    :sparkles: Implement Playback Options

--- Player.py
+++ Player.py
@@ -4,27 +4,72 @@ from Playlist import Playlist
 from Song import Song
 from utility import separator
 import progressbar
+import keyboard
 
 
 class Player:
+
+    widgets = ['Progress:', progressbar.Bar(
+        '#'), progressbar.Timer(format='%(elapsed)s')]
+    bar = progressbar.ProgressBar(widgets=widgets)
+
     def __init__(self) -> None:
         self.currentDuration = 0
+        self.currentSong = None
+        self.__handlers = []
+        self.options = {"p": ["pause", self.pause],
+                        "r": ["resume", self.resume],
+                        "s": ["stop", self.stop]
+                        }
+        self.pause = False
+        self.stop = False
+
+    def __enablePlaybackOptions(self):
+        for option in self.options.keys():
+            handler = keyboard.add_word_listener(
+                option, self.options[option][1], triggers=["enter"])
+            self.__handlers.append(handler)
+
+    def pause(self):
+        self.pause = True
+
+    def resume(self):
+        pass
+
+    def stop(self):
+        self.stop = True
+        self.currentDuration = 0
+        self.currentSong = None
+        for x in self.__handlers:
+            keyboard.remove_word_listener(x)
+
+    def play(self, song: Song):
+        while (song.duration != self.currentDuration):
+            if (self.pause):
+                inp = input("\nSong is paused..., press r to resume\n")
+                if (inp == 'r'):
+                    self.pause = False
+
+            elif (self.stop):
+                self.stop = False
+                break
+            else:
+                self.currentDuration += 0.1
+                sleep(1)
+                Player.bar.update(self.currentDuration)
 
     def playSong(self, song: Song):
+        self.currentSong = song
+        self.currentDuration = 0
         separator()
         print("Playing:", song.title, "by", ', '.join(song.artists))
-        print("Playback options: p:pause | r:resume | s:stop | n:next")
-        self.currentDuration = 0
 
-        widgets = ['Progress:', progressbar.Bar(
-            '#'), progressbar.Timer(format='%(elapsed)s'), f'/{song.duration}']
+        self.__enablePlaybackOptions()
+        print("Playback options:", end=" ")
+        for key in self.options.keys():
+            print(f'{key}:{self.options[key][0]}', end=" | ")
+        print()
 
-        bar = progressbar.ProgressBar(
-            max_value=song.duration, widgets=widgets).start()
+        Player.bar.max_value = song.duration
 
-        while self.currentDuration != song.duration:
-            sleep(1)
-            self.currentDuration += 0.1
-            if (self.currentDuration > song.duration):
-                break
-            bar.update(self.currentDuration)
+        self.play(song)
